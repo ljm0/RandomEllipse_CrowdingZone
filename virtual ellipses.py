4ed1cc3f0f6b9a05c2be365b42ad6190c87537a9
Miao Li <chuoli223@yahoo.com>
2018-11-01 23:44:19 +0100

    Add files via upload

--- virtual ellipses.py
+++ virtual ellipses.py
@@ -2,8 +2,7 @@
 """
 Spyder Editor
 
-Miao Li 
-This is the first version
+Miao Li
 """
 from psychopy import locale_setup, gui, visual, core, data, event, logging, sound, monitors
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
@@ -87,7 +86,7 @@ def defineVirtualEllipses(coordinate, ka = 0.02, kb = 0.05): # parameter for a a
 disk_posi = random.choice(positions)
 trgt_disk.setPos(disk_posi) 
 trgt_disk.draw()
-win.flip()
+#win.flip()
 core.wait(0.80)
 print (disk_posi)
 
@@ -97,24 +96,76 @@ virtual_e_radius = defineVirtualEllipses(disk_posi)
 virtual_e = Ellipse(Point(disk_posi[0],disk_posi[1]),virtual_e_radius[0],virtual_e_radius[1])
 print (virtual_e)
 
-
+'''
 #second disk
 taken_posi = [disk_posi]
-for i in range (0, N_disks):
+#for i in range (0, N_disks):
+disk_posi_new = random.choice(positions)
+while disk_posi_new in taken_posi:
+    disk_posi_new = random.choice(positions)
+
+virtual_e_radius_2 = defineVirtualEllipses(disk_posi_new)     
+virtual_e_2 = Ellipse(Point(disk_posi_new[0],disk_posi_new[1]),virtual_e_radius[0],virtual_e_radius[1])
+
+if len(virtual_e_2.intersection(virtual_e)): #inspect intersection between two virtual ellipes
     disk_posi_new = random.choice(positions)
     while disk_posi_new in taken_posi:
         disk_posi_new = random.choice(positions)
-    
-    virtual_e_radius_2 = defineVirtualEllipses(disk_posi_new)     
-    virtual_e_2 = Ellipse(Point(disk_posi_new[0],disk_posi_new[1]),virtual_e_radius[0],virtual_e_radius[1])
-    
-    if len(virtual_e_2.intersection(virtual_e)):
-          
-   
+    #print(disk_posi_new)
     taken_posi.append(disk_posi_new)
-  
-'''      
-     
+    trgt_disk.setPos(disk_posi_new)
+    trgt_disk.draw()
+    win.flip()
+else:
+    taken_posi.append(disk_posi_new)
+    #print(disk_posi_new)
+    trgt_disk.setPos(disk_posi_new)
+    trgt_disk.draw()
+    win.flip()
+
+
+
+'''
+def caclulateNewList (random_disk_coordinate, taken_list): # (新生成的随机点，已经保存的点坐标list)
+    
+    virtual_e_radius_2 = defineVirtualEllipses(random_disk_coordinate)
+    virtual_e_2 = Ellipse(Point(random_disk_coordinate[0],random_disk_coordinate[1]),virtual_e_radius_2[0],virtual_e_radius_2[1])
+    
+    for exist_n in range (0, len(taken_list)):
+        
+        exist_e_radius = defineVirtualEllipses(taken_list[exist_n])
+        exist_e = Ellipse(Point(taken_list[exist_n][0],taken_list[exist_n][1]), exist_e_radius[0],exist_e_radius[1])
+        
+        if len(virtual_e_2.intersection(exist_e)): #inspect intersection between two virtual ellipes
+            continue
+        else:
+            taken_list.append(random_disk_coordinate)
+            
+    return taken_list  #final list of position I want
+
+
+
+
+#disks_loop from the second disk
+
+taken_posi = [disk_posi]
+
+random_N = 0
+while random_N < N_disks:
+#for random_N in range(0, N_disks):   
+    disk_posi_new = random.choice(positions)
+    while disk_posi_new in taken_posi:
+        disk_posi_new = random.choice(positions)
+    taken_posi.append(disk_posi_new)
+    print(taken_posi)
+    
+    new_list = caclulateNewList(disk_posi_new,taken_posi)
+    random_N = random_N+1
+    
+print (new_list)
+        
+   
+         
         
 
 
