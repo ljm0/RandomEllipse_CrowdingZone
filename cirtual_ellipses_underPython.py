cb43092cf376c0cb229e94a803c3c9d76da94751
Miao Li <chuoli223@yahoo.com>
2018-11-19 18:05:12 +0100

    Add files via upload

--- cirtual_ellipses_underPython.py
+++ cirtual_ellipses_underPython.py
@@ -16,11 +16,11 @@ import random
 
 #some variables
 disk_radius = 5
-N_disks = 33
+#N_disks = 33
 
 # a list of posible positions
-grid_dimention_x = 152
-grid_dimention_y = 84   
+grid_dimention_x = 30#152
+grid_dimention_y = 30#84   
 
 linelength = 10
 start_x = -0.5*linelength*grid_dimention_x + 0.5*linelength
@@ -32,8 +32,7 @@ for x_count in range(0, grid_dimention_x):
     for y_count in range(0, grid_dimention_y):
         new_y = start_y + y_count*linelength
         positions.append((new_x, new_y))
-
-
+random.shuffle(positions)
 
 def defineVirtualEllipses(coordinate, ka = 0.25, kb = 0.1): # parameter for a and b; 
     '''This function defines the virtual ellipse. coordinate: the center of the ellipse
@@ -48,7 +47,19 @@ def defineVirtualEllipses(coordinate, ka = 0.25, kb = 0.1): # parameter for a an
 
 
 #first random disk
-disk_posi = random.choice(positions)
+disk_posi = positions[0] #random.choice(positions)
+positions.pop(0)
+
+
+def checkPosiOnEllipse( h, k, x, y, a, b):
+    '''
+    Check a given point (x, y) is inside, outside or on the ellipse
+    centered (h, k), semi-major axis = a, semi-minor axix = b
+    '''
+    p = ((math.pow((x-h), 2) // math.pow(a, 2)) + (math.pow((y-k), 2) // math.pow(b, 2)))
+
+    return p #if p<1, inside
+
 
 #the virtual ellipses of the first disk
 virtual_e_radius = defineVirtualEllipses(disk_posi) 
@@ -56,8 +67,8 @@ virtual_e_radial = Ellipse(Point(disk_posi[0],disk_posi[1]),virtual_e_radius[0],
 virtual_e_tan = Ellipse(Point(disk_posi[0],disk_posi[1]),virtual_e_radius[1],virtual_e_radius[0])
 
 equ = virtual_e_radial.equation()
-
-
+virtual_e_radial_a = virtual_e_radial.hradius
+virtual_e_radial_b = virtual_e_radial.vradius
 
 def caclulateNewList (random_disk_coordinate, taken_list): # (新生成的随机点，已经保存的点坐标list) # new random disk corrdinate, previous disk corrdinates list
     '''This function generate the final list that contains a group of disks coordinate. 
@@ -73,32 +84,46 @@ def caclulateNewList (random_disk_coordinate, taken_list): # (新生成的随机
         exist_e = Ellipse(Point(taken_list[exist_n][0], taken_list[exist_n][1]), exist_e_radius[0],exist_e_radius[1]) #perivous ellipses       
         
         if virtual_e_2.intersection(exist_e): #inspect intersection between two virtual ellipes
-            return [0]
+            positions.pop(0)
+            return [0]#breakout the function and  go into the while loop to delate this position
         else:
-            continue
+            continue    
+    
     taken_list.append(random_disk_coordinate)
+    #delate the the current position from the list positions and the corrosponding ellipses points.
+    
+    positions.pop(0)
+    temp_num = 0
+    for NPosition in positions:
+        judge = checkPosiOnEllipse(random_disk_coordinate[0], random_disk_coordinate[1], NPosition[0],NPosition[1],virtual_e_2.hradius,virtual_e_2.vradius) 
+        if judge <= 1:
+            positions.pop(temp_num)
+        temp_num = temp_num + 1
     return taken_list  #final list of position I want
 
-def checkPosiOnEllipse( h, k, x, y, a, b):
-    '''
-    Check a given point (x, y) is inside, outside or on the ellipse
-    centered (h, k), semi-major axis = a, semi-minor axix = b
-    '''
-    p = ((math.pow((x-h), 2) // math.pow(a, 2)) + (math.pow((y-k), 2) // math.pow(b, 2)))
+'''
+if new点和takenlist里的一个点相交
+  删去该positions点，得到新点
 
-    return p #if p<1, inside
+else 不相交
+ 取下一个takenlist的点，直到和所有已知的takenlist都不相交，
+  则把该点加入到takenlist,并删除positions里的该点，并删除该点周围的椭圆格点
+'''
+
 
 
 taken_posi = [disk_posi]
-while 1:
+while len(positions) > 0:
 #for random_N in range(0, N_disks):   
-    disk_posi_new = random.choice(positions)
-    while disk_posi_new in taken_posi:
-        disk_posi_new = random.choice(positions)
+    disk_posi_new = positions[0] 
+
+    
     #taken_posi.append(disk_posi_new)
-    #print("taken posi", taken_posi)
+    print("taken posi", taken_posi)
     new_list = caclulateNewList(disk_posi_new,taken_posi)
+    #delate the position
+    '''  
     if len(new_list) > N_disks:
         break  #final list of position I want
+    '''
 print ("new", new_list)
-
