be45f5417155b861f9540bccb616628aedda13b7
Flaviana-psy <chuoli223@yahoo.com>
2019-03-14 13:00:14 +0100

    [idea2b] -new position is inspected with both radial and tangential previous ellipeses. -cross ellipses as a whole -do not differentiate area1 with area 2 , and area3 with area4. -add random extra disk, but only one.

---
+++ run_virtual_ellipese_idea2b.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 17:28:02 2019
+
+@author: MiaoLi
+"""
+
+
+import os
+import time
+
+i_loop=1
+
+start = time.time()
+while(i_loop <= 10):
+    os.system('python virtual_ellipses_Idea2b.py' + ' ' + str(i_loop))
+    i_loop += 1
+end = time.time()
+
+print('time', str(end-start))
